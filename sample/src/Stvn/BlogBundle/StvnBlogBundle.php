420a5023e6b9793a7e8cc0a4ef7578cd7c9563ec
MicHaeLann <michael@Michaels-MacBook-Pro.local>
2016-04-20 15:48:49 +1200

    Remove sample

--- sample/src/Stvn/BlogBundle/StvnBlogBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Stvn\BlogBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class StvnBlogBundle extends Bundle
-{
-}
--- sample/src/Stvn/UserBundle/StvnUserBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Stvn\UserBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class StvnUserBundle extends Bundle
-{
-}
